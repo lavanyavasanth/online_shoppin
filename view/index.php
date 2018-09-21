db6f3337280c4e764fe7f410ff6ad947348906ac
Lavanya <lavanyavasanth1988@gmail.com>
2018-09-21 19:23:29 +1000

    indentation

--- view/index.php
+++ view/index.php
@@ -1,6 +1,5 @@
 
        <?php 
-//testing git
           include("pages/links.php");
 //          include("../control/display_db.php");
           include("pages/header.php");   
@@ -9,14 +8,5 @@
           include("pages/footer.php"); 
       ?> 
 
-<!--
-    <footer class="text-right">
-        
-            <a class="nav-link" href="#" ><i class="fab fa-facebook"></i><span class="sr-only">Facebook</span></a>
-              <a class="nav-link" href="#" ><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a>
-              <a class="nav-link" href="#" ><i class="fab fa-instagram"></i><span class="sr-only">Instgram</span></a>
-        
-        </footer>
--->
 
 
