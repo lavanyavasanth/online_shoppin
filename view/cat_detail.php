6a8b1b3583fda08aa2d48a41b57f88ba1221dd86
Lavanya <lavanyavasanth1988@gmail.com>
2018-09-21 19:22:37 +1000

    deleting unwanted lines

--- view/cat_detail.php
+++ view/cat_detail.php
@@ -1,10 +1,8 @@
 
        <?php 
           include("pages/links.php");
-//          include("../control/display_db.php");
           include("pages/header.php");   
           include("pages/navbar.php"); 
-          echo "<div id='bodyleft'><ul>";cat_detail();echo"</ul></div>";
-//          include("pages/bodyleft.php");    
+          echo "<div id='bodyleft'><ul>";cat_detail();echo"</ul></div>";    
           include("pages/footer.php");
       ?> 
